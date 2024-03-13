2ee5dbec086649c7b13eb382f4bd4d367ee8622f
GsteXD <142363261+GsteXD@users.noreply.github.com>
2024-03-13 19:35:18 -0300

    Update CPF.c

--- CPF.c
+++ CPF.c
@@ -2,54 +2,7 @@
 #include<stdbool.h>
 #include<string.h>
 
-int
-calculaDigito (int cpf[], int mult[], int N)
-{
-  int i, soma = 0;
-  for (i = 0; i < N; i++)
-	{
-	  soma = soma + cpf[i] * mult[i];
-	}
-  int resto = ((soma * 10) % 11) % 10;
-  return resto;
-}
-
-bool
-verificaNAORepetido (int cpf[])
-{
-  for (int i = 1; i < 11; i++)
-	{
-	  if (cpf[0] != cpf[i])
-		{
-		  return false;
-		}
-	}
-  return true;
-}
-
-void
-OrigemCPF (int cpf[])
-{
-  char regioes[10][100] =		//matriz de caracteres determinando as 10 regiões e o números de caracteres máximo(100)
-  {
-	"* 0 Rio Grande do Sul",
-	"* 1 Distrito Federal, GoiC!s, Mato Grosso, Mato Grosso do Sul e Tocantins",
-	"* 2 Amazonas, ParC!, Roraima, AmapC!, Acre e RondC4nia",
-	"* 3 CearC!, MaranhC#o e PiauC-",
-	"* 4 ParaC-ba, Pernambuco, Alagoas e Rio Grande do Norte",
-	"* 5 Bahia e Sergipe",
-	"* 6 Minas Gerais",
-	"* 7 Rio de Janeiro e EspC-rito Santo",
-	"* 8 SC#o Paulo",
-	"* 9 ParanC! e Santa Catarina",
-  };
-  printf ("\n%s", regioes[cpf[8]]);
-}
-
-int
-main ()
-{
-
+int main () {
   int cpf[11];
   int m1[9] = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
   int m2[10] = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
@@ -63,23 +16,57 @@ main ()
   				//stdin significa que o programa está pegando inputs do teclado
 	
   tam = strlen (entrada); //Atribuí o tamanho da string em "entrada" e guarda em "tam"
-  for (i = 0; i < tam; i++)
-	{
-	  if (entrada[i] >= 48 && entrada[i] <= 57) //Verifica se os valores apresentados correspondem com os números de 0 a 9 da tabela ASCII.
-		{
+  for (i = 0; i < tam; i++){
+	  if (entrada[i] >= 48 && entrada[i] <= 57){ //Verifica se os valores apresentados correspondem com os números de 0 a 9 da tabela ASCII.
 		  cpf[j] = entrada[i] - 48; //Pega o valor convertido pela "entrada" em ASCII e subtraC- 48 para dar o valor em decimal
 		  j++; //passa para a próxima posição de CPF
 		}
 	}
 
-  if (calculaDigito (cpf, m1, 9) == cpf[9]
-	  && calculaDigito (cpf, m2, 10) == cpf[10]
-	  && verificaNAORepetido (cpf) == false)
-	{
+  if (calculaDigito (cpf, m1, 9) == cpf[9] && calculaDigito (cpf, m2, 10) == cpf[10] && verificaNAORepetido (cpf) == false){
 	  printf ("\nCPF válido.");
 	  OrigemCPF (cpf);
 	}
   else
 	printf ("\nErro no CPF");
   return 0;
+}
+
+int calculaDigito (int cpf[], int mult[], int N){
+  int i, soma = 0;
+  for (i = 0; i < N; i++)
+	{
+	  soma = soma + cpf[i] * mult[i];
+	}
+  int resto = ((soma * 10) % 11) % 10;
+  return resto;
+}
+
+bool verificaNAORepetido (int cpf[]) {
+  for (int i = 1; i < 11; i++)
+	{
+	  if (cpf[0] != cpf[i])
+		{
+		  return false;
+		}
+	}
+  return true;
+}
+
+void
+OrigemCPF (int cpf[]) {
+  char regioes[10][100] = //matriz de caracteres determinando as 10 regiões e o números de caracteres máximo(100)
+  {
+	"* 0 Rio Grande do Sul",
+	"* 1 Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins",
+	"* 2 Amazonas, Pará. Roraima, Amapá, Acre e Rondônia",
+	"* 3 Ceará, Maranhão e Piauí",
+	"* 4 Paraíba, Pernambuco, Alagoas e Rio Grande do Norte",
+	"* 5 Bahia e Sergipe",
+	"* 6 Minas Gerais",
+	"* 7 Rio de Janeiro e Espírito Santo",
+	"* 8 São Paulo",
+	"* 9 Paraná e Santa Catarina",
+  };
+  printf ("\n%s", regioes[cpf[8]]);
 }
